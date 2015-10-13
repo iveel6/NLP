f030b2de1c627b8d2e02b25ddc6e04be11dd44ad
iveel <iveel6@yahoo.com>
2015-10-12 22:44:32 -0400

    coding

---
+++ topic_model_em/test.py
@@ -0,0 +1,9 @@
+import sys, re, collections
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+from operator import itemgetter
+
+p = collections.defaultdict(lambda:np.zeros(3))
+print p[1][2]
+print len(p[2]);
